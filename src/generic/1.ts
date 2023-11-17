5cdef730ff364ddfe209f792045c55132a193eb5
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-11-17 11:21:11 +0200

    finish HW2

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,15 +3,14 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
-  return new Promise((resolve) => {
-    resolve(['Text', 50]);
-  });
+function getPromise<T extends (string | number)[]>() {
+	return new Promise<T>((resolve) => {
+		resolve(['Text', 50] as T);
+	});
 }
 
-getPromise()
-.then((data) => {
-  console.log(data);
+getPromise().then((data) => {
+	console.log(data);
 });
 
 export {};
